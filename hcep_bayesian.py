03f647e3307be61600c9e91f3f2ec8005aca8583
Madeleine Snyder <madeleine.snyder.48@gmail.com>
2017-02-06 11:15:01 -0500

    added writeup file

--- hcep_bayesian.py
+++ hcep_bayesian.py
@@ -7,6 +7,7 @@ start_time = time.time()
 
 zero_idx = []
 allZeros = []
+mse = []
 
 trainData = pd.read_csv('../train.csv.gz',compression="gzip")
 train_y = trainData.loc[:,"gap"]
@@ -17,17 +18,30 @@ zero_idx = train_x.apply(lambda x: np.all(x==0))
 for i, value in enumerate(zero_idx):
 	if value == True:
 		allZeros.append(i)
-	if i ==True:
-		print(zero_idx.iloc[i])
-print(zero_idx.iloc)
-print(zero_idx[i]==True for i in zero_idx)
-print(train_x.iloc[zero_idx[i]==True])
+
+# Drop all the useless columns from the data matrix
+# for m in range(1000000):
+# 	if (train_x.iloc[m,1] == 1.0):
+# 		print('no')
+
+# print(train_x.loc[:,1].name)
+
+# train_x.drop(train_x.loc[:,[i for i in allZeros]].name,axis=1)
 
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
 # Make the k-fold thingy
-k = 8
+k = 8 # number folds
+n = 10000 # fold size
 
-#1. Randomly choose 20000
+for trial in range(k):
+	sample = np.random.choice(train_x.shape[0],2*n) # choose 2000 random values
+	reg = linear_model.LinearRegression()
+	# fit the model to X (first 10000 rows and all colums of the random sample from train_x) and y (same 10000 rows from the labeled dataset)
+	reg.fit(train_x[sample[0:n],:],train_y[sample[0:n]])
+	# predict using the validation data in the model and get mse
+	xhat = predict(train_x[sample[n:-1],:]) 
+	# append mse
+	mse.append(sum([(train_y[sample[n:-1]][i] - xhat[i])**2 for i in range(n-1)])/float(n-1))
 
